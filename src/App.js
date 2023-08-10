5398a50d3a53d52cb22900942503a09b8d626c53
Md. Mohiuddin Khaled Maruf <maru@stibodx.com>
2023-08-10 22:53:06 +0600

    componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,26 +6,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Khaled",
+      monsters: [],
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => {
+      this.setState(
+        () => {
+          return { monsters: users }
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button
-            onClick={() =>
-              this.setState(() => ({
-                name: "Maruf",
-              }))
-            }
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster, index) => {
+          return <h1 key={index}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
